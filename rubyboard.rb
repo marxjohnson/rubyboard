cf35fd2db2e669ad4579706dc684f9c458c4ace2
Mark Johnson <mark@barrenfrozenwasteland.com>
2012-01-14 23:23:10 +0000

    Added comments

--- rubyboard.rb
+++ rubyboard.rb
@@ -7,11 +7,13 @@ EVENT_DETECT = 32
 EVENT_CONTACT = 1
 
 begin
+	# Check the board's ID appears in lsusb
 	lsusb = `lsusb`
 	if lsusb.split("\n").select{|l| l.include? "2047:ffe7"}.count == 0
 		raise "Boogie Board Rip not currently connected"
 	end
 
+	# Find the hidraw device assined the the board
 	begin
 		dmesg = `dmesg`
 		bbdev = dmesg.split("\n").select{|l| l.match "2047:FFE7.*hidraw[0-9]"}.pop.match("(hidraw[0-9]+)").to_s
@@ -23,6 +25,7 @@ begin
 	events = []
 	threads = []
 
+	# Start a thread to read each 32 byte event from the device and store it in a queue
 	thread = Thread.new do
 		board = File.new("/dev/"+bbdev, 'r')
 		while true do
@@ -34,13 +37,17 @@ begin
 	end
 	threads << thread
 
+	# Start a thread to read each event from the queue and process it
 	thread = Thread.new do
+		# Create an array to store the current and previous events, so we can see what
+		# changes each time.
 		previous = Array.new(32, 0)
 		current = []
 		while true do
 			current = events.shift
 			next if current.nil?
 
+			# Byte 11 Tells us the state of the buttons
 			if current[10] != previous[10]
 				state = current[10]
 				pstate = previous[10]
@@ -60,6 +67,7 @@ begin
 					puts "Wake Pressed"
 				end
 			end
+			# Byte 4 tells us if the pen is in range, and whether it's touching the screen
 			if current[3] != previous[3]
 				state = current[3]
 				pstate = previous[3]
@@ -74,6 +82,8 @@ begin
 					puts "Pen contact detected"
 				end
 			end
+			# With the board portrait, bytes 4 and 5 are the y co-ordinate of the pen, 
+			# 6 and 7 are the x co-ordinate, and the origin is the top-right corner.
 			cury = current[4]+(current[5]*256)
 			curx = current[6]+(current[7]*256)
 			prey = previous[4]+(previous[5]*256)
